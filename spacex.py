1ee968bcbc7a43d1878a0b2e7628df0c08fa3de1
djeck1432 <djeck1432@users.noreply.github.com>
2019-12-16 22:47:11 +0100

    Update spacex.py

--- spacex.py
+++ spacex.py
@@ -1,24 +1,28 @@
 import requests
 from pathlib import Path
 
-Path('images').mkdir(parents=True, exist_ok=True)
-PATH= Path('images')
+
 
 
 def fetch_spacex_last_launch():
   for image_number,image in enumerate(spacex_image_links):
-    filename = 'spacex{number}.jpg'.format(number=str(image_number+1))
+    filename = 'spacex{number}.jpg'.format(number=image_number+1)
     response = requests.get(image)
     response.raise_for_status()
     with open(PATH/filename,mode='wb') as image:
       image.write(response.content)
 
+
+
 if __name__ == "__main__":
+  Path('images').mkdir(parents=True, exist_ok=True)
+  PATH = Path('images')
+
   url = 'https://api.spacexdata.com/v3/launches/latest'
   params = {
-    'pretty':'true',
+    'pretty': 'true',
   }
-  response = requests.get(url,params=params)
+  response = requests.get(url, params=params)
   response.raise_for_status()
   spacex_image_links = response.json()['links']['flickr_images']
   fetch_spacex_last_launch()
